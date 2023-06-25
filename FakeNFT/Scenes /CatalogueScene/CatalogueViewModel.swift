a7b2a2376bbd2cf35b34f719e00a8010fa781bb6
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-06-25 12:40:03 +0300

    add catalogueViewModel and catalogueDataProvider

---
+++ FakeNFT/Scenes /CatalogueScene/CatalogueViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  CatalogueViewModel.swift
+//  FakeNFT
+//
+//  Created by Konstantin Kirillov on 25.06.2023.
+//
+
+import Foundation
+
+final class CollectionListViewModel {
+	var collections: [CollectionItem] = []
+	private let provider: CatalogueProviderProtocol
+	
+	init(provider: CatalogueProviderProtocol) {
+		self.provider = provider
+		collections = provider.getCollections()
+	}
+	
+	func setFilterByCount() {
+		collections = collections.sorted(by: { $0.count > $1.count })
+	}
+	
+	func setFilterByName() {
+		collections = collections.sorted(by: { $0.title > $1.title })
+	}
+}
