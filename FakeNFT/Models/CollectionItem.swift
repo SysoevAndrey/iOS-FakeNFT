7e2f13c27e6bf8b735796e397768067aa40c5685
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-06-25 12:28:35 +0300

    add catalogue cell

---
+++ FakeNFT/Models/CollectionItem.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionItem.swift
+//  FakeNFT
+//
+//  Created by Konstantin Kirillov on 25.06.2023.
+//
+
+import Foundation
+
+struct CollectionItem {
+	let imageStringURL: String
+	let title: String
+	let count: Int
+}
